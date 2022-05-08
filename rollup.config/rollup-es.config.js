a7aa9ea7dbb5009b79d2d461f908d0000cd9155e
zhangzhengsmiling <zhangzhengsmiling@163.com>
2022-05-08 12:43:46 +0800

    feat: css bundle

--- rollup.config/rollup-es.config.js
+++ rollup.config/rollup-es.config.js
@@ -3,12 +3,12 @@ import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
 import path from 'path'
 import typescript from 'rollup-plugin-typescript2'
-import { terser } from 'rollup-plugin-terser';
-import less from 'rollup-plugin-less';
-import fs from 'fs';
-import _less from 'less';
-import uglifyCss from 'uglifycss';
-const CURRENT_WORKSPACE_DIRECTORY = process.cwd();
+import { terser } from 'rollup-plugin-terser'
+import less from 'rollup-plugin-less'
+import fs from 'fs'
+import _less from 'less'
+import uglifyCss from 'uglifycss'
+const CURRENT_WORKSPACE_DIRECTORY = process.cwd()
 
 const pathResolve = (_path) => {
   const fullPath = path.resolve(CURRENT_WORKSPACE_DIRECTORY, _path)
@@ -59,7 +59,7 @@ const plugins = [
   terser(),
   commonjs(),
   less({
-    output: 'css/main.css',
+    output: 'rollup-es/style.css',
     insert: false,
   }),
 ]
@@ -70,68 +70,72 @@ const entry = pathResolve('src')
 // read all files of a directory
 const filesOf = (dir) => {
   const readFileOfDir = (dir, results) => {
-    const list = fs.readdirSync(dir)
-      .map(item => path.resolve(dir, item));
+    const list = fs.readdirSync(dir).map((item) => path.resolve(dir, item))
     list.forEach((_path) => {
-      const stats = fs.statSync(_path);
+      const stats = fs.statSync(_path)
       if (stats.isDirectory()) {
-        readFileOfDir(path.resolve(_path), results);
+        readFileOfDir(path.resolve(_path), results)
       } else {
-        results.push(_path);
+        results.push(_path)
       }
     })
   }
-  const result = [];
-  readFileOfDir(dir, result);
-  return result;
+  const result = []
+  readFileOfDir(dir, result)
+  return result
 }
 
 const create = (p) => {
-  const parsed = path.parse(p);
-  fs.mkdirSync(parsed.dir, { recursive: true });
+  const parsed = path.parse(p)
+  fs.mkdirSync(parsed.dir, { recursive: true })
 }
 
+const toString = (d) => d.toString()
+const readFileSync = (filename) => fs.readFileSync(filename)
+
 const less2css = (lessFiles) => {
-  const toString = (d) => d.toString();
-  const readFileSync = (filename) => fs.readFileSync(filename);
-  const d = lessFiles.map(p => path.resolve(CURRENT_WORKSPACE_DIRECTORY, p))
+  const d = lessFiles
+    .map((p) => path.resolve(CURRENT_WORKSPACE_DIRECTORY, p))
     .map(readFileSync)
     .map(toString)
-    .map(content => _less.render(content))
+    .map((content) => _less.render(content, { rewriteUrls: true }))
   return Promise.all(d)
-  .then((cssResults) => {
-    return cssResults.map((d, i) => [
-      lessFiles[i]
-        .replace(CURRENT_WORKSPACE_DIRECTORY, '')
-        .replace('/src/', '')
-        .replace(/.less$/, ''),
-      d
-    ])
-  })
-  .then(data => {
-    data.map(([name, content]) => {
-      const filename = path.resolve(CURRENT_WORKSPACE_DIRECTORY, 'es-css', name + '.css');
-      create(filename);
-      fs.writeFileSync(filename, uglifyCss.processString(content.css));
+    .then((cssResults) => {
+      return cssResults.map((d, i) => [
+        lessFiles[i]
+          .replace(CURRENT_WORKSPACE_DIRECTORY, '')
+          .replace('/src/', '')
+          .replace(/.less$/, ''),
+        d,
+      ])
     })
-  })
+    .then((data) => {
+      data.map(([name, content]) => {
+        const filename = path.resolve(CURRENT_WORKSPACE_DIRECTORY, 'rollup-es', name + '.css')
+        create(filename)
+        fs.writeFileSync(filename, uglifyCss.processString(content.css))
+      })
+    })
 }
 
 const esBundler = async () => {
-  const allFiles = filesOf(path.resolve(CURRENT_WORKSPACE_DIRECTORY, 'src'));
-  const tsFiles = allFiles.filter(p => /.tsx$|.ts$/.test(p));
-  const lessFiles = allFiles.filter(p => /.less$/.test(p));
+  const allFiles = filesOf(path.resolve(CURRENT_WORKSPACE_DIRECTORY, 'src'))
+  const tsFiles = allFiles.filter((p) => /.tsx$|.ts$/.test(p))
+  const lessFiles = allFiles.filter((p) => /.less$/.test(p))
 
-  // const res = filesOf(path.resolve(CURRENT_WORKSPACE_DIRECTORY, 'src'))
-  //   .filter(p => !/.less$/.test(p));
-  const pathParser = (path) => path.replace(CURRENT_WORKSPACE_DIRECTORY, '')
-    .replace('/src/', '')
-    .replace(/.ts$|.tsx$/, '')
+  const pathParser = (path) =>
+    path
+      .replace(CURRENT_WORKSPACE_DIRECTORY, '')
+      .replace('/src/', '')
+      .replace(/.ts$|.tsx$/, '')
 
-  const input = tsFiles.reduce((temp, source) => ({
-    ...temp,
-    [pathParser(source)]: source,
-  }), {})
+  const input = tsFiles.reduce(
+    (temp, source) => ({
+      ...temp,
+      [pathParser(source)]: source,
+    }),
+    {},
+  )
 
   const sourceConfigs = {
     input,
@@ -139,16 +143,13 @@ const esBundler = async () => {
       {
         dir: path.resolve(CURRENT_WORKSPACE_DIRECTORY, 'rollup-es'),
         format: 'es',
-      }
+      },
     ],
     plugins,
     external: ['react'],
   }
-  
   await less2css(lessFiles)
-  return sourceConfigs;
-
+  return sourceConfigs
 }
 
-
-export default Promise.all([esBundler()]);
+export default Promise.all([esBundler()])
